ee31b844f9043e127934e3af9331e6e0a31ef363
Student User <student@workstation.lab.example.com>
2020-03-12 13:25:21 +1100

    test

--- friends/index.php
+++ friends/index.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-print "<h1>List of Friends v8.1</h1>";
+print "<h1><font color=red>List of Friends v9</font></h1>";
 
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = $conn->query($sql);
